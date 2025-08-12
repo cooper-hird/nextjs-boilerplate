b107538e0dd57ace5e4806e5c7e2b40a8a306059
Cooper Hird <124203742+cooper-hird@users.noreply.github.com>
2025-08-12 16:13:50 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
+        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+          <Navbar />
+          {children}
+          <Footer />
+          <Analytics />
+          <SpeedInsights />
+        </main>
+      </body>  
         {children}
       </body>
     </html>
