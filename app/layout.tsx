ac7f728d48bd54325b86da13fbe9d10e571e61bf
Cooper Hird <124203742+cooper-hird@users.noreply.github.com>
2025-08-12 16:15:38 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,14 +19,18 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html
+      lang="en"
+      className={cx(
+        'text-black bg-white dark:text-white dark:bg-black',
+        GeistSans.variable,
+        GeistMono.variable
+      )}
+    >
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
@@ -35,9 +39,7 @@ export default function RootLayout({
           <Analytics />
           <SpeedInsights />
         </main>
-      </body>  
-        {children}
       </body>
     </html>
-  );
+  )
 }
