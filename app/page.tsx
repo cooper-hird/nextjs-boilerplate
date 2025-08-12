279ac8076b23700f916f53d63c15105647219697
Cooper Hird <124203742+cooper-hird@users.noreply.github.com>
2025-08-12 16:17:57 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,8 @@ export default function Home() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`Hi there! This is my personal website for all the things I like to work on and share with the world. 
+        Thanks for checking it out!`}
       </p>
     </section>
   );
